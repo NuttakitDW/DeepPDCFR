8afffeb0b4d405b6d9cd98cb7820fdd03d4e6747
Lattawit Kulanuwat <sususa003@gmail.com>
2026-02-08 22:53:59 +0700

    chore: update reget training step logging

--- deeppdcfr/bulk_model.py
+++ deeppdcfr/bulk_model.py
@@ -600,7 +600,7 @@ class BulkRegretTrainer:
                 self.imm_optimizer.step()
 
             last_loss = loss.item()
-            if logger and (step % 50 == 0 or step == self.train_steps - 1):
+            if logger and (step % 1 == 0 or step == self.train_steps - 1):
                 logger.info(
                     f"[{step}/{self.train_steps}] regret loss: {loss.item():.6f}, "
                     f"imm: {imm_loss.item():.6f}"
